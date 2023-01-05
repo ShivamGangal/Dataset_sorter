39f81a71eacedcd96664c96699b6769f03d569e8
ShivamGangal <shivam.gangal@gmail.com>
2023-01-04 22:59:10 -0500

    adjusted code to become class variable agnostic

--- dataset_sorter.py
+++ dataset_sorter.py
@@ -1,4 +1,3 @@
-# Cat dog horse sorter
 import pathlib
 import argparse
 import os
@@ -11,47 +10,45 @@ def get_args():
     args=parser.parse_args()
     return args
 
+def get_class_name(self,specific_file):
+    specific_file_class = specific_file.split('.')[0]
+    if '-' in specific_file_class:
+        specific_file_class = specific_file_class.split('-')[0]
+    return specific_file_class
+
 def collect_stats(dir):
-    stats={'cat_num':0,'dog_num':0,'horse_num':0,'rider_num':0}
+    stats={}
     files = os.listdir(dir)
     for specific_file in files:
-        if "cat" in specific_file:
-            stats['cat_num']=+1
-        elif "dog" in specific_file:
-            stats['dog_num']=+1
-        elif "horse" in specific_file:
-            stats['horse_num']=+1
-        elif "rider" in specific_file:
-            stats['rider_num']=+1
-    return
-def make_and_move(args,files):
-    cat_dir=os.path.join(args.target_dir, "cat_picture")
-    dog_dir=os.path.join(args.target_dir, "dog_picture")
-    horse_dir=os.path.join(args.target_dir, "horse_picture")
-    rider_dir=os.path.join(args.target_dir, "rider_picture")
-    os.mkdir(cat_dir)
-    os.mkdir(dog_dir)
-    os.mkdir(horse_dir)
-    os.mkdir(rider_dir)     # Making Complete
+        specific_file_class = self.get_class_name(specific_file)        
+        if specific_file_class not in stats:
+            stats[specific_file_class]=1
+        else:
+            stats[specific_file_class]+=1
+    return stats
 
+def make_and_move(args,stats):
+    keys=list(stats.keys())
+    directories={}
+    for k in keys:
+        directories[k] = os.path.join(args.target_dir,k)
+        if os.path.isdir(directories[k]):
+            pass
+        else:
+            os.mkdir(directories[k])   
+   
+    files=os.listdir(args.input_dir)
     for specific_file in files:
-        if "cat" in specific_file:
-            shutil.copy(os.path.join(args.input_dir,specific_file),cat_dir)
-        elif "dog" in specific_file:
-            shutil.copy(os.path.join(args.input_dir,specific_file),dog_dir)
-        elif "horse" in specific_file:
-            shutil.copy(os.path.join(args.input_dir,specific_file),horse_dir)
-        elif "rider" in specific_file:
-            shutil.copy(os.path.join(args.input_dir,specific_file),rider_dir)
+        specific_file_class = specific_file.split('.')[0]
+        if specific_file_class in specific_file:
+            shutil.copy(os.path.join(args.input_dir,specific_file),directories[specific_file_class])
 
     return
 
 def main():
     args = get_args()
-    files=os.listdir(args.input_dir)
-    collect_stats(args.input_dir)
-    make_and_move(args,files)
-    #Move function
+    stats = collect_stats(args.input_dir)
+    make_and_move(args,stats)
     return
 
 if __name__ == "__main__":
